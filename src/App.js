1a41f03891cc857c052519990ebc5ad5191e1b0d
EnterVPL <woramowy@gmail.com>
2020-09-12 11:59:47 +0200

    Made Calculator component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Button } from './components/Button';
 import { Calculator } from './components/Calulator';
-
-const day = (new Date).toTimeString();
 
 function App() {
   return (
     <div className="App">
-		<p>Hello</p>
-		<p>{day}</p>
 		<Calculator />
     </div>
   );
