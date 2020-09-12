c5b87bc8d41b3d29326ee0d326923001a91e9b59
EnterVPL <woramowy@gmail.com>
2020-09-12 12:51:33 +0200

    Select component

--- src/components/Calulator/Calculator.js
+++ src/components/Calulator/Calculator.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+
+function Select(props) {
+	return (
+		<select defaultValue={props.val} onChange={(event) => props.change(event.target.value)}>
+			<option value="USD">USD</option>
+			<option value="PLN">PLN</option>
+			<option value="GBP">GBP</option>
+			<option value="EUR">EUR</option>
+		</select>
+	);
+}
 
 function Calculator () {
+	const [amount, setAmount] = useState(0);
+	const [currencyFrom, setCurrencyFrom] = useState('PLN');
+	const [currencyTo, setCurrencyTo] = useState('USD');
+
 	return (
 		<form>
 			<div>
-				<input type="number" placeholder="Amount" onChange={(event) => console.log(event.target.value)} />
+				<input type="number" placeholder="Amount" onChange={(event) => setAmount(event.target.value)} />
 			</div>
 			<div>
 				<span>From:</span>
-				<select onChange={(event) => console.log(event.target.value)}>
-					<option value="USD">USD</option>
-					<option value="PLN">PLN</option>
-					<option value="GBP">GBP</option>
-					<option value="EUR">EUR</option>
-				</select>
+				<Select val={currencyFrom} change={setCurrencyFrom} handler="" />
 			</div>
 			<div>
 				<span>To:</span>
-				<select>
-					<option value="USD">USD</option>
-					<option value="PLN">PLN</option>
-					<option value="GBP">GBP</option>
-					<option value="EUR">EUR</option>
-				</select>
+				<Select val={currencyTo} change={setCurrencyTo} />
 			</div>
-			<div>Result: <span> --- </span></div>
+			<div>Result: <span> {amount} </span></div>
 		</form>
 	);
 }
