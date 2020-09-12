91dda839ec5338c98b4a0e452fcace7e6a25bb86
EnterVPL <woramowy@gmail.com>
2020-09-12 13:29:46 +0200

    fetching data

--- src/components/Calulator/Calculator.js
+++ src/components/Calulator/Calculator.js
@@ -1,14 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from '../Button';
 
+const url = 'https://api.ratesapi.io/api/latest/';
 
 function Select(props) {
+	const [rates, setRates] = useState([]);
+
+	useEffect(() => {
+		fetch(`${url}?base=PLN`)
+		.then(response => response.json())
+		.then(data => {
+			setRates(Object.keys(data.rates));
+		});
+		return () => {
+			//clearInterval
+			//removeEventListerner
+		}
+	}, []);
+
 	return (
-		<select defaultValue={props.val} onChange={(event) => props.change(event.target.value)}>
-			<option value="USD">USD</option>
-			<option value="PLN">PLN</option>
-			<option value="GBP">GBP</option>
-			<option value="EUR">EUR</option>
+		<select value={props.val} onChange={(event) => props.change(event.target.value)}>
+			{rates.map((elem, index) => <option key={`curr-${elem}`} value={elem}>{elem}</option>)}
 		</select>
 	);
 }
@@ -17,11 +29,16 @@ function Calculator () {
 	const [amount, setAmount] = useState(0);
 	const [currencyFrom, setCurrencyFrom] = useState('PLN');
 	const [currencyTo, setCurrencyTo] = useState('USD');
+	const [result, setResult] = useState('---');
 
 	const handlerSubmit = (event) => {
 		event.preventDefault();
 		console.log(event);
-		fetch('https://api.ratesapi.io/api/latest?base=PLN');
+		fetch(`${url}?base=${currencyFrom}`)
+		.then(response => response.json())
+		.then(data => {
+			setResult(data.rates[currencyTo] * amount);
+		});
 	}
 
 	return (
@@ -37,7 +54,7 @@ function Calculator () {
 				<span>To:</span>
 				<Select val={currencyTo} change={setCurrencyTo} />
 			</div>
-			<div>Result: <span> {amount} </span></div>
+			<div>Result: <span> {result} </span></div>
 			<Button>Send</Button>
 		</form>
 	);
