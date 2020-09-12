d28b77888641845ad6912557e7bbdd353aca0fc0
EnterVPL <woramowy@gmail.com>
2020-09-12 13:52:47 +0200

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import './App.css';
-import { Calculator } from './components/Calulator';
+
+import {
+	BrowserRouter as Router, Switch, Link, Route
+} from 'react-router-dom'
+
+import Calculator from './pages/Calculator';
+import Contact from './pages/Contact';
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
-		<Calculator />
+		<Router>
+			<ul>
+				<li><Link to="/">Home</Link></li>
+				<li><Link to="/about">About</Link></li>
+				<li><Link to="/contact">Contact</Link></li>
+			</ul>
+			<Switch>
+				<Route path="/" exact>
+					<Calculator />
+				</Route>
+				<Route path="/about">
+					<Contact />
+				</Route>
+				<Route path="/contact">
+					<About />
+				</Route>
+			</Switch>
+		</Router>
     </div>
   );
 }
