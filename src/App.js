76daa5975e3d68ef76176d182bdfc8c20d4106ae
EnterVPL <woramowy@gmail.com>
2020-09-12 10:53:14 +0200

    Made "hello"

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const day = (new Date).toTimeString();
+
+function Button() {
+	return (
+		<button>Click me!!!!!</button>
+	);
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<p>Hello</p>
+		<p>{day}</p>
+		<Button />
+		<Button />
     </div>
   );
 }
